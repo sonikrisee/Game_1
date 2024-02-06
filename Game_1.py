e43ca0b87aa9a08fb224634f3ae24ae7c83e4306
sonikrisee <sonik.rise@gmail.com>
2024-02-06 22:01:35 +0300

    Game_1

--- Game_1.py
+++ Game_1.py
@@ -3,9 +3,8 @@ import sys
 
 points = 100
 bet = int(0)
-
+print("Количество очков ", points)
 while True:
-    print("Количество очков ", points)
 
     if points == 0 and bet == 0:
         print("У вас не осталось очков")
@@ -27,25 +26,29 @@ while True:
             roll2 = random.randrange(1, 6)
             sum = roll1 + roll2
             print("Сумма очков броска компьютера равна: ", sum)
-            if sum < 7 and a < 7:
+
+            if sum == a:
+                bet = bet * 4
                 points = points + bet
-                print("Вы выйграли")
+                print("Вы выйграли +", bet)
+
+            elif sum < 7 and a < 7:
+                points = points + bet
+                print("Вы выйграли +", bet)
 
             elif sum > 7 and a > 7:
                 points = points + bet
-                print("Вы выйграли")
-            elif sum == a:
-                bet = bet * 4
-                points = points + bet
-                print("Вы выйграли")
+                print("Вы выйграли +", bet)
+
             else:
                 points = points - bet
-                print("Вы проиграли")
+                print("Вы проиграли -", bet)
 
+            print("Количество очков ", points)
             print("Хотите продолжить игру?")
             x = input("Для продолжения введите 'Да', для выхода введите 'Нет' ")
             if x == 'Да':
-                
+                print("Играем дальше")
             else:
                 sys.exit()
 
